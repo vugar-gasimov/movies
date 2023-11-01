5b43d8b43dd7f4b308c081b1d7f24270ef25b208
vugar-gasimov <vuqar5qasimov@gmail.com>
2023-11-01 14:47:24 +0200

    ++=

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,3 +1,17 @@
 export const App = () => {
-  return <div></div>;
+  return (
+    <div
+      style={{
+        height: '100vh',
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        fontSize: 40,
+        color: '#010101',
+        boxShadow: '0 0 5px rgba(0, 0, 0, 0.2)', // Fixed box-shadow syntax
+        background:
+          'linear-gradient(315deg, rgb(255, 159, 14), rgb(228, 64, 187), rgb(101, 90, 220), rgb(72, 140, 251), rgb(72, 140, 251), rgb(41, 219, 188), rgb(221, 245, 5))',
+      }}
+    ></div>
+  );
 };
